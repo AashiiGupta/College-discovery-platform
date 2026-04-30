711031b98b57435d7f5e1bd9214e6629344dcfff
Aashi Gupta <130901149+AashiiGupta@users.noreply.github.com>
2026-05-01 02:13:47 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,6 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    "https://college-discovery-platform-pied.vercel.app"
     credentials: true,
   })
 );
